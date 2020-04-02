01e673b92ffd42b348ca89ca44bc4c3f86223ee3
DakshinD <50462931+DakshinD@users.noreply.github.com>
2020-04-02 16:09:14 -0400

    Create SwapitySwapitySwap.cpp

---
+++ USACO/Silver/February 2020/SwapitySwapitySwap.cpp
@@ -0,0 +1,49 @@
+//http://www.usaco.org/index.php?page=viewproblem2&cpid=1014
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+
+typedef long long ll;
+typedef long double ld;
+
+
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define endl '\n'
+const ll INF = 1e18;
+#define all(x) x.begin(), x.end()
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+
+int main() {
+//CHECK LIMITS U RARTED DUMBO - 10^9 too SLO for BF
+//FIND PATTERN IF TOO SLOW FOR BRUTE FORCE
+//CHECK ALL PARAMETERS
+//MAKE SURE YOU FIGURE OUT WHICH YOU USE (int vs ll)
+//WRITE SOL ON PAPER BFR IMPLEMENTING
+//WRITE YOUR OWN TEST CASES TO DEAL WITH EDGE CASES
+/*    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("loan.in", "r", stdin);
+    freopen("loan.out", "w", stdout);*/
+//-------------------------------------------------
+
+
+    return 0;
+}
+//Sum formula n^1 = n(n+1)/2
+//Sum formula n^2 = n(n+1)(2n+1)/6
+//Arithmetic (Constant) Progression = n(a+b)/2
+//Geometric Progression = (bk-a)/(k (ratio difference)-1)
+//1+2+4+8+...+2^(n−1) = 2^n − 1.
