9f79dde331f39609bd67cf6d19488ff8451ff71c
DakshinD <50462931+DakshinD@users.noreply.github.com>
2020-04-05 17:36:59 -0400

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef priority_queue<int> pq;
+typedef priority_queue<int, vi, greater<int>> mpq;
+
+const ll INF = 1e18;
+const int mod = 1e9+7;
+const int dX[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define ft front()
+#define bk back()
+#define IOS  ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a) for(int i = 0; i < a; i++)
+#define FoR(i, a) for(int i = 1; i <= a; i++)
+#define ROF(i, a) for (int i = (a)-1; i >= 0; i--)
+
+int main(){
+    IOS;
+    /* freopen("-.in", "r", stdin); */
+    /* freopen("-.out", "w", stdout); */  
+//-------------------------------------------------
+
+    return 0;
+    // Read notes to self
+}
+
+/* NOTES TO SELF:
+ * Check for int vs ll, worst case just switch everything to ll
+ * Check for array bounds if you SEGFAULT, especially in range based loops
+ * Always initialize everything in global 
+ * Edge cases such as n = 1 or 0
+ * Make sure to calculate big O complexity
+ * You are geniosity you got this
+ */
