891778eba6384f43f73c19bcdc3e52cef7168f69
DakshinD <50462931+DakshinD@users.noreply.github.com>
2020-04-09 23:26:21 -0400

    Update BIT.cpp

--- Algorithms/BIT.cpp
+++ Algorithms/BIT.cpp
@@ -1,18 +1,18 @@
-struct BIT{
+struct BIT{ //remember to stay consistent whether you one index or 2 index, never use 0 indexing
     int bit[ARRAY];
     void c(){
-        for(int i=0;i<=n;i++){
-            bit[i]=0;
+        for(int i = 0; i <= n; i++){
+            bit[i] = 0;
         }
     }
     void update(int x, int y){
-        for(;x<=n;x+=x&-x)
-            bit[x]+=y;
+        for(;x <= MAXN; x += x & -x) //MAXN being a huge number, the restriction provided
+            bit[x] += y;
     }
     int query(int x){
-        int sum=0;
-        for(;x>0;x-=x&-x){
-            sum+=bit[x];
+        int sum = 0;
+        for(;x > 0; x -= x & -x){
+            sum += bit[x];
         }
         return sum;
     }
