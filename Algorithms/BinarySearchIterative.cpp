9dfef1ed3de13205e4db3f539d1af27b263c7db3
DakshinD <50462931+DakshinD@users.noreply.github.com>
2020-05-19 20:33:31 -0500

    Update BinarySearchIterative.cpp

--- Algorithms/BinarySearchIterative.cpp
+++ Algorithms/BinarySearchIterative.cpp
@@ -1,14 +1,26 @@
-int low = 0; int right = n;
-int val = 0; //make this value looking for
-while(low <= right){
-  int mid = (low + (right - low))/2;
-  if(arr[mid] == mid){
-    return mid;
+//find smallest value for which P(x) holds true
+while (l < r) {
+  int mid = (l + r) / 2;
+  if (P(mid)) r = mid;
+  else l = mid + 1;
+} // after the loop, l = r = x
+//might not work if P(r) = 0, so extend to r+1 or pre check
+
+//find largest value for which P(x) holds true - we could just set Q(x) != P(x) and use above to find x+1
+//or use below implementation
+while (l < r) {
+  int mid = (l + r + 1) / 2;
+  if (P(mid)) l = mid;
+  else r = mid - 1;
+} // after the loop, l = r = x
+
+//if you need to find occurrence of k in sorted array, modified 1st version
+bool search(int x[], int n, int k) {
+  int l = 0, r = n - 1;
+  while (l < r) {
+    int mid = (l + r) / 2;
+    if (x[mid] >= k) r = mid; 
+    else l = mid + 1;
   }
-  if(array[mid] > val){
-    right = mid - 1;
-  }
-  else{
-    low = mid + 1;
-  }
+  return x[l] == k;
 }
