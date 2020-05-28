0aa1430265b2479657dde94245fc80f2531c517f
DakshinD <50462931+DakshinD@users.noreply.github.com>
2020-05-28 18:45:39 -0400

    Update SparseTable.cpp

--- Algorithms/SparseTable.cpp
+++ Algorithms/SparseTable.cpp
@@ -8,33 +8,37 @@ for (int i = 0; i < N; i++)
 for (int j = 1; j <= K; j++)
     for (int i = 0; i + (1 << j) <= N; i++)
         st[i][j] = st[i][j-1] + st[i + (1 << (j - 1))][j - 1];
-       
-long long sum = 0; //sum of (l, r) in O(log MAXN)
-for (int j = K; j >= 0; j--) {
-    if ((1 << j) <= R - L + 1) {
-        sum += st[L][j];
-        L += 1 << j;
-    }
-}      
+
+int query(int l, int r){       
+    long long sum = 0; //sum of (l, r) in O(log MAXN)
+    for (int j = K; j >= 0; j--) {
+        if ((1 << j) <= R - L + 1) {
+            sum += st[L][j];
+            L += 1 << j;
+        }
+    }      
+    return sum;
+}
 
 //range min implementation
 //precomputaton of logs
 
 int log[MAXN+1];
+int st[MAXN][K];
+
+//precompute logs
 log[1] = 0;
 for (int i = 2; i <= MAXN; i++)
     log[i] = log[i/2] + 1;
-    
-// construction
-int st[MAXN][K];
-
+//construction
 for (int i = 0; i < N; i++)
     st[i][0] = array[i];
-
 for (int j = 1; j <= K; j++)
     for (int i = 0; i + (1 << j) <= N; i++)
-        st[i][j] = min(st[i][j-1], st[i + (1 << (j - 1))][j - 1]);
-        
+        st[i][j] = min(st[i][j-1], st[i + (1 << (j - 1))][j - 1]);   
 //query in O(1)
-int j = log[R - L + 1];
-int minimum = min(st[L][j], st[R - (1 << j) + 1][j]);
+int query(int l, int r){
+    int j = logs[r - l +1];
+    int minimum = min(sparse[l][j], sparse[r-(1<<j)+1][j]);
+    return minimum;
+}
