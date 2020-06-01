9e12cf6edc3a36ed068881f89c8086eb26daf653
DakshinD <50462931+DakshinD@users.noreply.github.com>
2020-06-01 17:23:52 -0400

    Update SegmentTree.cpp

--- Algorithms/SegmentTree.cpp
+++ Algorithms/SegmentTree.cpp
@@ -1,63 +1,73 @@
-int SZ;
-vector<int> seg;
-int arr[];
+template<class T>
+class SegTree {
+public:
+    int SZ;
+    vector<T> seg;
 
-void setSize(int sz){
-    SZ = sz;
-    seg.resize(SZ*4);
-}
-void build(int sz){
-    SZ = sz;
-    seg.resize(SZ*4);
-    bld(1, 0, SZ-1);
-}
-int query(int a, int b){
-    return qry(a, b, 1, 0, SZ-1);
-}
-void update(int idx, int newVal){
-    upd(1, 0, SZ-1, idx, newVal);
-}
-//now for the actual functions
-int f(int a, int b){
-    return a + b; //change this for different problems, sum query, RMQ, etc.
-}
-void bld(int node, int l, int r) {
-    if (l > r)
-        return;
-    if (l == r) {
-        seg[node] = arr[l];
-        return;
+    SegTree(int sz) {
+        SZ = sz;
+        seg.resize(SZ * 4);
     }
-    int mid = (l + r) / 2;
-    build(node * 2, l, mid, arr);
-    build(node * 2 + 1, mid + 1, r, arr);
-    seg[node] = f(seg[node * 2], seg[node * 2 + 1]);
-}
-int qry(int a, int b, int node, int l, int r) {
-    if (l > b || r < a) {
-        return -1;
+
+    SegTree(int sz, T arr[]) {
+        SZ = sz;
+        seg.resize(SZ * 4);
+        build(1, 0, SZ - 1, arr);
     }
-    if (l >= a && r <= b) {
-        return seg[node];
+
+    T query(int a, int b) {
+        return query(a, b, 1, 0, SZ - 1);
     }
-    int mid = (l + r) / 2;
-    int n1 = query(a, b, node * 2, l, mid);
-    int n2 = query(a, b, node * 2 + 1, mid + 1, r);
-    if (n1 == -1)
-        return n2;
-    if (n2 == -1)
-        return n1;
-    return f(n1, n2);
-}
-void upd(int node, int l, int r, int idx, int newVal) {
-    if (idx < l || idx > r)
-        return;
-    if(l == r) {
-        seg[node] = newVal;
-        return;
+
+    void update(int idx, T newVal) {
+        update(1, 0, SZ - 1, idx, newVal);
     }
-    int mid = (l + r) / 2;
-    update(node * 2, l, mid, idx, newVal);
-    update(node * 2 + 1, mid + 1, r, idx, newVal);
-    seg[node] = f(seg[node*2], seg[node*2+1]);
-}
+
+private:
+    T f(T a, T b) { //change for different problems
+        return a + b;
+    }
+
+    void build(int node, int l, int r, T arr[]) {
+        if (l > r)
+            return;
+        if (l == r) {
+            seg[node] = arr[l];
+            return;
+        }
+        int mid = (l + r) / 2;
+        build(node * 2, l, mid, arr);
+        build(node * 2 + 1, mid + 1, r, arr);
+        seg[node] = f(seg[node * 2], seg[node * 2 + 1]);
+    }
+
+    T query(int a, int b, int node, int l, int r) {
+        if (l > b || r < a) {
+            return NINF;
+        }
+        if (l >= a && r <= b) {
+            return seg[node];
+        }
+        int mid = (l + r) / 2;
+        int n1 = query(a, b, node * 2, l, mid);
+        int n2 = query(a, b, node * 2 + 1, mid + 1, r);
+        if (n1 == NINF)
+            return n2;
+        if (n2 == NINF)
+            return n1;
+        return f(n1, n2);
+    }
+
+    void update(int node, int l, int r, int idx, T newVal) {
+        if (idx < l || idx > r)
+            return;
+        if(l == r) {
+            seg[node] = newVal;
+            return;
+        }
+        int mid = (l + r) / 2;
+        update(node * 2, l, mid, idx, newVal);
+        update(node * 2 + 1, mid + 1, r, idx, newVal);
+        seg[node] = f(seg[node*2], seg[node*2+1]);
+    }
+};
